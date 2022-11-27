278962158f4cfb40088a7e634adaf309347e3d92
Dan Bock <danbock@gmail.com>
2022-11-27 17:14:24 -0500

    Make it slightly functional

--- src/highlight.ts
+++ src/highlight.ts
@@ -3,12 +3,15 @@ const highlight = () => {
   const pvs = getBox()?.getElementsByClassName('pv')
   for (let i = 0; i < (pvs?.length || 0); i++) {
     const sans = pvs?.item(i)?.getElementsByClassName('pv-san')
-    for (let j = 0; j < (sans?.length || 0); j++) {
-      if (j % 2 === 0) {
-        const el = sans?.item(j) as HTMLElement
-        if (el) el.style.backgroundColor = 'green'
-      }
-    }
+
+    const evens = Array.from(sans || []).reduce<HTMLElement[]>(
+      (acc, cur, j) => [...acc, ...(j % 2 === 0 ? [cur as HTMLElement] : [])],
+      [],
+    )
+
+    evens.forEach(e => {
+      e.style.backgroundColor = 'pink'
+    })
   }
 }
 
