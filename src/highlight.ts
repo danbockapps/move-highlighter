3dcdf511635d3eead2b714075c51121bf4143c76
Dan Bock <danbock@gmail.com>
2022-11-27 17:01:40 -0500

    Highlight the evens only

--- src/highlight.ts
+++ src/highlight.ts
@@ -1,9 +1,14 @@
 import getBox from './getBox'
 const highlight = () => {
-  const els = getBox()?.getElementsByClassName('pv-san')
-  for (let i = 0; i < (els?.length || 0); i++) {
-    const el = els?.item(i) as HTMLElement
-    if (el) el.style.backgroundColor = 'yellow'
+  const pvs = getBox()?.getElementsByClassName('pv')
+  for (let i = 0; i < (pvs?.length || 0); i++) {
+    const sans = pvs?.item(i)?.getElementsByClassName('pv-san')
+    for (let j = 0; j < (sans?.length || 0); j++) {
+      if (j % 2 === 0) {
+        const el = sans?.item(j) as HTMLElement
+        if (el) el.style.backgroundColor = 'green'
+      }
+    }
   }
 }
 
